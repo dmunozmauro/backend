e8f662797b64ff9c248ed34ec81b21eb1b521a8d
dmunozmauro <40881574+dmunozmauro@users.noreply.github.com>
2018-11-13 19:51:08 -0300

    'nuevos-cambios'

--- src/mysql/mysql.ts
+++ src/mysql/mysql.ts
@@ -1,26 +1,27 @@
-import mysql from 'mysql';
+import mysql = require('mysql');
 
 export default class MySQL {
 
     cnn: mysql.Connection;
     conect: boolean = false;
+    private static _instance: MySQL;
 
     constructor() {
         console.log('BD iniciada');
         this.cnn = mysql.createConnection({
             host: 'localhost',
-            user: 'root',
-            password: '',
+            user: 'node_user',
+            password: '123456',
             database: 'node_db'
         });
 
-        this.cnn.connect()
+        this.cnn.connect();
     }
 
     private conectarDB() {
-        this.cnn.connect((error: mysql.MysqlError) => {
-            if (error) {
-                console.log(error.message);
+        this.cnn.connect((errorDb: mysql.MysqlError) => {
+            if (errorDb) {
+                console.log(errorDb.message);
                 return;
             }
             this.conect = true;
